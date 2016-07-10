c9fa725d9d62ce6a475f7ba3dc2a8c8238466635
johntech-o <zhoukunta@qq.com>
2016-07-10 16:34:04 +0800

    pprof refactor

--- pprof/pprof.go
+++ pprof/pprof.go
@@ -10,48 +10,97 @@ func init() {
 	Proc.Init()
 }
 
-var lastNumGc uint32
+const (
+	TotalAlloc  = "TotalAlloc"
+	HeapObjects = "HeapObjects"
+	StackInuse  = "StackInuse"
+)
+
+type Stats struct {
+	index                int
+	content              map[int]map[string]int
+	lastNumGc            uint32
+	maxGoroutineNum      int
+	lastLoopGoroutineNum int
+}
+
+var memStats = func() *Stats {
+	return &Stats{content: make(map[int]map[string]int)}
+}()
+
+func MemStats() {
+	memStats.DoMemoryStats()
+}
+
+func ProcessStats() {
+	memStats.DoProcessStat()
+}
 
 // if gc happend
-func DoMemoryStats(output bool) {
+func (s *Stats) DoMemoryStats() {
+
 	var memoryStats runtime.MemStats
 	runtime.ReadMemStats(&memoryStats)
 	v := reflect.ValueOf(memoryStats)
 	t := reflect.TypeOf(memoryStats)
-	if lastNumGc != memoryStats.NumGC {
-		output = true
-		lastNumGc = memoryStats.NumGC
+	if s.lastNumGc != memoryStats.NumGC {
+		s.lastNumGc = memoryStats.NumGC
 	}
-	if output {
-		fmt.Println("---------------NumGC", memoryStats.NumGC, "------------------")
-		for i := 0; i < v.NumField(); i++ {
-			field := v.Field(i)
-			fieldType := t.Field(i)
-			switch fieldType.Name {
-			case "TotalAlloc", "HeapAlloc", "HeapObjects", "Mallocs", "StackInuse", "MSpanInuse", "NumGC":
-				fmt.Println(fieldType.Name, field.Interface())
-			case "PauseNs":
-				fmt.Println("PauseMs", float64(memoryStats.PauseNs[(memoryStats.NumGC+255)%256])/1e6)
-			case "PauseTotalNs":
-				fmt.Println("PauseTotalMs", float64(field.Interface().(uint64))/1e6)
+	s.content[s.index] = make(map[string]int)
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		fieldType := t.Field(i)
+		switch fieldType.Name {
+		case "TotalAlloc", "HeapAlloc", "HeapObjects", "Mallocs", "StackInuse", "MSpanInuse", "NumGC":
+			v := field.Interface()
+			switch t := v.(type) {
+			case uint32:
+				s.content[s.index][fieldType.Name] = int(v.(uint32))
+			case uint64:
+				s.content[s.index][fieldType.Name] = int(v.(uint64))
+			default:
+				fmt.Println(t)
 			}
+
+		case "PauseNs":
+			s.content[s.index]["PauseMs"] = int(memoryStats.PauseNs[(memoryStats.NumGC+255)%256]) / 1e6
+		case "PauseTotalNs":
+			s.content[s.index]["PauseTotalMs"] = int(field.Interface().(uint64)) / 1e6
 		}
-		fmt.Println("---------------------------------")
 	}
+	s.index += 1
 }
 
-var maxGoroutineNum int
-var lastLoopGoroutineNum int
-
-func DoProcessStat() {
-	n := runtime.NumGoroutine()
-	if n > maxGoroutineNum {
-		maxGoroutineNum = n
+func StatIncrement(keys ...string) {
+	for _, key := range keys {
+		if memStats.index == 1 {
+			fmt.Printf("%s: %d - %d = %d\n", key, memStats.content[memStats.index-1][key], 0, memStats.content[memStats.index-1][key])
+			continue
+		}
+		// fmt.Println(memStats.index-1, memStats.index-2)
+		// fmt.Println(memStats.content[memStats.index-1])
+		// fmt.Println(memStats.content[memStats.index-2])
+		res := memStats.content[memStats.index-1][key] - memStats.content[memStats.index-2][key]
+		fmt.Printf("%s: %d - %d = %d\n", key, memStats.content[memStats.index-1][key], memStats.content[memStats.index-2][key], res)
 	}
-	if n != lastLoopGoroutineNum {
-		fmt.Println("current goutines:", n)
-		fmt.Println("max goroutine num: ", maxGoroutineNum)
-		lastLoopGoroutineNum = n
+
+}
+
+func Current() string {
+	result := "---------------NumGC memStats.lastNumGc ------------------\n"
+	result = fmt.Sprintf("%v\n", memStats.content)
+	result += "-------------------------------------------------------"
+	return result
+}
+
+func (s *Stats) DoProcessStat() {
+	n := runtime.NumGoroutine()
+	if n > s.maxGoroutineNum {
+		s.maxGoroutineNum = n
+	}
+	if n != s.lastLoopGoroutineNum {
+		fmt.Printf("current goutines:%10d, max goroutine num: %10d\n", n, s.maxGoroutineNum)
+		s.lastLoopGoroutineNum = n
 		Proc.Cpu.Refresh()
 		Proc.Mem.Refresh()
 		fmt.Println("process cpu and mem: ", Proc.Cpu, Proc.Mem)
